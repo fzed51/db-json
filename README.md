4d77c46a24f28bc2831cc334ea7476121c42279a
Fabien Sanchez <fzed51+git@gmail.com>
2023-05-31 17:08:56 +0200

    feat / mise en place de DbJson

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # db-json
+
 Enregistre des informations dans un fichier json
+
+## Installation
+
+```
+yarn add @fzed51/db-json
+```
+
+ou
+
+```
+npm i @fzed51/db-json
+```
+
+## Usage
+
+```typescript
+interface Data {
+    id: number;
+    value: string;
+}
+
+const dbJsonOption = {
+  forceCreate: true,
+  logFullPath: false,
+}
+
+const db = new DbJson<Data>('file.json', dbJsonOption)
+(async ()=> {
+    const data = await db.get() // []
+
+    await db.set([{
+        id: 1,
+        value: "A"
+    }])
+
+    await db.add({
+        id: 2,
+        value: "B"
+    })
+
+    await db.replace({
+        id: 1,
+        value: "C"
+    }, e => e.id === 1)
+
+    await db.delete(e => e.id === 2)
+})()
+```
---
+++ src/DbJson.ts
@@ -0,0 +1,131 @@
+import { existsSync, writeFile, readFile } from "fs";
+import { resolve } from "path";
+
+type DbJsonOptions = {
+  forceCreate: boolean;
+  logFullPath: boolean;
+};
+
+/**
+ * DbJson est un objet qui permet d'enregistrer une collection d'objet dans
+ * un fichier json
+ */
+export class DbJson<T> {
+  protected readonly options: DbJsonOptions;
+  protected readonly filename: string;
+  constructor(filename: string, options: Partial<DbJsonOptions>) {
+    const defaultOptions: DbJsonOptions = {
+      forceCreate: false,
+      logFullPath: false,
+    };
+    this.options = {
+      ...defaultOptions,
+      ...options,
+    };
+
+    this.filename = resolve(filename);
+    if (this.options.logFullPath) {
+      console.log(this.filename);
+    }
+    if (!existsSync(this.filename)) {
+      if (this.options.forceCreate) {
+        writeFile(this.filename, "", function (err) {
+          if (err) throw err;
+          console.log("File is created successfully.");
+        });
+      } else {
+        throw new Error("Le fichier n'existe pas");
+      }
+    }
+  }
+
+  /**
+   * Retourne le fichier json sous forme de collection d'objet
+   * @returns T[]
+   */
+  async get(): Promise<T[]> {
+    return new Promise((resolve, reject) => {
+      readFile(this.filename, (err, data) => {
+        if (err) {
+          reject(err);
+        }
+        const content = data.toString();
+        if (content === "") {
+          resolve([]);
+          return;
+        }
+        resolve(JSON.parse(content));
+      });
+    });
+  }
+
+  /**
+   * Enregistre une collection d'objet dans le fichier json
+   * @param data T[]
+   * @returns T[]
+   */
+  async set(data: T[]): Promise<T[]> {
+    return new Promise((resolve, reject) => {
+      writeFile(this.filename, JSON.stringify(data, null, 2), (err) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(data);
+      });
+    });
+  }
+
+  /**
+   * Ajoute un element à la collection d'objet dans le fichier json
+   * @param element T
+   * @returns T[]
+   */
+  async add(element: T): Promise<T[]> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const data = await this.get();
+        const newData = [...data, element];
+        resolve(this.set(newData));
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
+
+  /**
+   * Remplace des éléments
+   */
+  async replace(
+    element: T,
+    filter: (e: T, i: number) => boolean
+  ): Promise<T[]> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const data = await this.get();
+        const newData = data.map((e: T, i: number) =>
+          filter(e, i) ? element : e
+        );
+        resolve(this.set(newData));
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
+
+  /**
+   * Remplace des éléments
+   */
+  async delete(filter: (e: T, i: number) => boolean): Promise<T[]> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const data = await this.get();
+        const newData = data.filter((e: T, i: number) => !filter(e, i));
+        resolve(this.set(newData));
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
+}
+
+export default DbJson;
